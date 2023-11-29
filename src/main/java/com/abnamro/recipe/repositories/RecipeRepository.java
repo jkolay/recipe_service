a021b96d8c7f245ad7f593470f8e185a2fee7ba6
jakolay <jayatii.kolaay@gmail.com>
2023-11-29 16:18:25 +0100

    corrected

--- src/main/java/com/abnamro/recipe/repositories/RecipeRepository.java
+++ src/main/java/com/abnamro/recipe/repositories/RecipeRepository.java
@@ -21,14 +21,14 @@ public interface RecipeRepository extends JpaRepository<RecipeDao, Integer>, Jpa
             "FROM Recipes r " +
             "LEFT JOIN recipe_ingredient ri ON r.id = ri.recipe_id " +
             "LEFT JOIN ingredients i ON ri.ingredient_id = i.id " +
-
             "WHERE (:isVegetarian IS NULL OR :isVegetarian IS TRUE OR r.type<> 'VEGETARIAN') " +
             "AND (:isVegetarian IS NULL OR :isVegetarian IS FALSE OR r.type = 'VEGETARIAN') " +
             "AND (:servings IS NULL OR r.number_of_servings = :servings) " +
             "AND (:ingredientIn IS NULL OR lower(i.ingredient) = lower(:ingredientIn)) " +
             "AND (:ingredientEx IS NULL OR r.id NOT IN (SELECT DISTINCT ri2.recipe_id FROM recipe_ingredient ri2, ingredients i2 " +
-            "WHERE ri2.ingredient_id = i2.id and lower(i2.ingredient) = lower(:ingredientEx))) " +
-            "AND (:text IS  NULL ) OR lower(r.instructions)  LIKE CONCAT('%',lower(:text),'%') " +
+            "WHERE ri2.ingredient_id = i2.id  " +
+            "and lower(i2.ingredient) = lower(:ingredientEx)))"+
+            "AND (:text IS  NULL  OR lower(r.instructions)  LIKE CONCAT('%',lower(:text),'%')) " +
             "ORDER BY r.id", nativeQuery = true)
     List<RecipeDao> search(@Param("isVegetarian") Boolean isVegetarian,
                         @Param("servings") Integer servings,
